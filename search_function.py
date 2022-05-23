97fd2e19cb362957aaf2b5fa6e6555db227a437d
sbacci99 <simone.bacci99@gmail.com>
2022-05-23 20:40:03 +0200

    commiting search function

---
+++ search_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+    def search(self, label, keyword, out_label=None):
+        mask = self[label].str.contains(keyword, case=False, na=False).values
+        if out_label is not None:
+            out = self[out_label][mask]
+        else:
+            out = self[label][mask]
+        return out, mask
