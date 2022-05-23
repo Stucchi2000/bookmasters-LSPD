b0edf7669a687215c58b08af72876fb3d45cc8f7
Filippo <filippo.ricco@student.h-farm.com>
2022-05-23 23:00:32 +0200

    fixed search_function

--- search_function.py
+++ search_function.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
     def search(self, label, keyword, out_label=None):
-        mask = self[label].str.contains(keyword, case=False, na=False).values
+        mask = self[label].str.contains(keyword, case=False, na=False, regex=False).values
         if out_label is not None:
             out = self[out_label][mask]
         else:
