782f9b6cdab211ceb50ef9ed8d9b5acb48f0e9c2
Filippo <filippo.ricco@student.h-farm.com>
2022-05-26 21:17:24 +0200

    Refining PEP-(

--- get_info.py
+++ get_info.py
@@ -7,16 +7,17 @@ import pandas as pd
 from IPython.display import display
 # import seaborn as sb
 
+
 class BookRecords():
     
-
     def get_info(self, title, info):
         # Check existence of column label
         if title not in self['title'].values:
             out = self.search('title', title)
             raise AssertionError(f'Book title {title} not present in dataset...\n\nDid you mean {out[0].values}???')
-        summ = self[info][self['title']==title].values
+        summ = self[info][self['title'] == title].values
         print(f'--Book Name: \n\t{title}\n\n--{info}: \n\t {summ}\n\n')      
+
 
 def main():
     
